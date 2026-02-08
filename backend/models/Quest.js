512d3bfd74475039db4718504d70f6e31832aea9
Senal Perera <senalperera777@gmail.com>
2026-02-08 15:59:33 +0530

    Schema setup update
    - made schema for Quest collection
    - made schema collection containing logged trips by the user

---
+++ backend/models/Quest.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const questSchema = new mongoose.schema({
+  qid: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  instructions: {
+    type: String,
+    required: true,
+  },
+  xpCount: {
+    type: Number,
+    required: true,
+  },
+  // Foreign key
+  location: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Location",
+    required: [true, "A quest must belong to a specific location"],
+  },
+  questType: {
+    type: String,
+    required: true,
+  },
+  difficulty: {
+    type: String,
+    enum: ["Easy", "Medium", "Hard"],
+    message: "{VALUE} is not a valid difficulty level",
+    default: "Easy",
+  },
+});
+
+const Quest = mongoose.model("Quest", questSchema);
+
+module.exports = Quest;
