7f0c82b8a509ce77f5fc6bdd4b0124739c999874
Hiffan Abdul Gaffoor <mohamed.20241059@iit.ac.lk>
2026-03-15 01:07:01 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,13 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:voyant/app.dart';
-
-import 'package:voyant/main.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:user_repository/user_repository.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(FirebaseUserRepo()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
