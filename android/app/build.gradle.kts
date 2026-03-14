dda4e301b7f7bb4bf6f9365497393804aeca607b
Thinuka Rambukwella <thinukaananda@gmail.com>
2026-03-15 01:39:55 +0530

    Fixing Jvm Target issue ( CI/CD)
    Fixed the gradle task failure by updating the jvm target ( previously was a to string method ) and updated local properties

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,8 @@ plugins {
     id("com.google.gms.google-services")
 }
 
+import java.util.Properties
+
 dependencies {
 
     implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
@@ -23,7 +25,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -37,7 +39,7 @@ android {
         versionName = flutter.versionName
 
         // Manifest placeholders for API keys
-        val localProperties = java.util.Properties()
+        val localProperties = Properties()
         val localPropertiesFile = rootProject.file("local.properties")
         if (localPropertiesFile.exists()) {
             localProperties.load(localPropertiesFile.inputStream())
