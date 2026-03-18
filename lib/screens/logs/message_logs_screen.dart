bb0bba547516754ebc6e91d5fe63afd7f6a3d974
Thinuka Rambukwella <thinukaananda@gmail.com>
2026-03-18 16:12:19 +0530

    feat formatted time stamp in message content
    formatted timestamp to display in a just now format in the message content for easier user readability

--- lib/screens/logs/message_logs_screen.dart
+++ lib/screens/logs/message_logs_screen.dart
@@ -599,3 +599,24 @@ with TickerProviderStateMixin {
       ),
     );
   }
+
+  //timestamp -> time ago format conversion 
+   String _formatTimestamp(DateTime timestamp) {
+    final now = DateTime.now();
+    final difference = now.difference(timestamp);
+
+    if (difference.inMinutes < 1) {
+      return "Just now";
+    } else if (difference.inHours < 1) {
+      return "${difference.inMinutes}m ago";
+    } else if (difference.inDays < 1) {
+      return "${difference.inHours}h ago";
+    } else if (difference.inDays < 7) {
+      return "${difference.inDays}d ago";
+    } else {
+      return "${timestamp.day}/${timestamp.month}/${timestamp.year}";
+    }
+  }
+}
+
+
