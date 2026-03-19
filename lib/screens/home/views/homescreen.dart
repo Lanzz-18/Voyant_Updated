24db838fac814d49cc74896ba62afea097e8aaf9
Hiffan Abdul Gaffoor <mohamed.20241059@iit.ac.lk>
2026-03-19 13:16:31 +0530

    Changed leaderboard icon to be placed above map

--- lib/screens/home/views/homescreen.dart
+++ lib/screens/home/views/homescreen.dart
@@ -16,11 +16,11 @@ class Homescreen extends StatelessWidget {
       appBar: AppBar(
         actions: [
           IconButton(
-            icon: const Icon(Icons.leaderboard),
+            icon: const Icon(Icons.person),
             onPressed: () {
               Navigator.of(context).push(
                 MaterialPageRoute(
-                  builder: (context) => const LeaderboardScreen(),
+                  builder: (context) => const AvatarScreen(),
                 ),
               );
             },
@@ -48,9 +48,31 @@ class Homescreen extends StatelessWidget {
         ],
         
       ),
-      body: const Map(),
+      body: Stack(
+        children: [
+          const Map(),
+          Positioned(
+            top: 16,
+            left: 16,
+            child: SafeArea(
+              child: FloatingActionButton(
+                heroTag: 'leaderboard_btn',
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (context) => const LeaderboardScreen(),
+                    ),
+                  );
+                },
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                child: const Icon(Icons.leaderboard, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
       // Add a button to go to class selection screen
-      
+
       floatingActionButton: FloatingActionButton.extended(
         onPressed: () {
           // Dispatch the logout event
