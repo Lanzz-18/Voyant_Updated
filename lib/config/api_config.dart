85ad55ac9d98d231367d3fbb7e949de403350ff1
Hiffan Abdul Gaffoor <mohamed.20241059@iit.ac.lk>
2026-04-19 11:14:38 +0530

    Fix: changed links in api config

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,9 +1,9 @@
 class ApiConfig {
   // TODO: Update this URL after deploying to Render
-  static const String _baseUrl = 'https://voyant-backend.onrender.com/api';
+  static const String _baseUrl = 'https://voyant-0f9w.onrender.com/api';
   
   static String get baseUrl {
     // Render backend URL - update with your actual Render URL
-    return 'https://voyant-backend.onrender.com/api';
+    return 'https://voyant-0f9w.onrender.com/api';
   }
 }
