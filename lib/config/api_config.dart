c71628a40d19046bf1b1d104bc0a1e8f622ccd58
Senal Perera <senalperera777@gmail.com>
2026-04-19 10:30:58 +0530

    fix: update local hosting to hosted backend

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,10 +1,9 @@
 class ApiConfig {
-  static const String _baseUrl = 'https://voyant-server.vercel.app/api';
+  static const String _hosted = 'https://voyant-server.vercel.app/api';
   static const String _localAndroidEmulator = 'http://10.0.2.2:3000/api';
 
-  static String get baseUrl => _localAndroidEmulator;
-  /*static String get baseUrl {
-    //using emulator url
-    return 'https://voyant-server.vercel.app/api';
-  }*/
+  // flip this when needed
+  static const bool useHosted = true;
+
+  static String get baseUrl => useHosted ? _hosted : _localAndroidEmulator;
 }
